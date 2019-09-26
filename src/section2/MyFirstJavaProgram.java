5e6c283feb32bc437d1c341fcc2ddef6585c5e8d
DerekHSA <55466846+DerekHSA@users.noreply.github.com>
2019-09-25 18:34:17 -0700

    Add files via upload

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,17 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
+		// START HERE 
+	Robot RealmBot = new Robot();
+	RealmBot.changeRobot("https://www.pngkey.com/png/full/94-941719_naruto-png-library-stock-naruto-shippuden-render.png");
+		RealmBot.setSpeed(99);
+		RealmBot.penDown();
+		RealmBot.setPenColor(Color.CYAN);
+		RealmBot.setPenWidth(5000000);
+		for(int i =0; i<4; i++) {
+		RealmBot.move(341);
+		RealmBot.turn(90);
 		
-		
+		}
 	}
 }
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,27 +10,28 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot Naruto = new Robot();
 		// 3. Put the robot's pen down
-
+		Naruto.penDown();
 		// 8. Make the robot go at maximum speed (100)
-
+		Naruto.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-
+		Naruto.setPenColor(Color.cyan);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+		int Side = 5;
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
+		int Angle = 360/Side;
 		// 7. Use a for loop to repeat the steps below (#2, #10, #6, #11) 200 times. When this is done you should see a pentagon
-
+		for(int i=0; i<600; i++) {
 			// 2. Move the robot 200 pixels
-	
+			Naruto.move(i);
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 	
 			// 6. Turn the robot the amount in your angle variable
-	
+			Naruto.turn(Angle);
 			// 11. Turn the robot one more degree
-
+			Naruto.turn(1);
+		}
 	}
 
 	// Variations:
