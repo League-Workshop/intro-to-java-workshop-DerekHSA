1038607efd20e90f36427c4d8a61d606144580f6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-04 17:59:38 -0800

    Game

--- src/section4/Game1.java
+++ src/section4/Game1.java
@@ -4,14 +4,13 @@ import javax.swing.JOptionPane;
 public class Game1 {
 public static void main(String[] args) {
 	JOptionPane.showMessageDialog(null, "Chose your foe.");
-	String Foe=JOptionPane.showInputDialog("Dragon(Easy)\nHydra(Medium)\nDragon Lord(Hard)\nWhat is the difference?");
+	String Foe=JOptionPane.showInputDialog("Dragon(Easy)\nHydra(Medium)\nDragon Lord(Hard)");
 	if(Foe.equalsIgnoreCase("Dragon")) {
+		String Name=JOptionPane.showInputDialog(null, "This dragon shoudn't have messed with ____!");
 		
 	}else if(Foe.equalsIgnoreCase("Hydra")) {
 		
 	}else if(Foe.equalsIgnoreCase("Dragon Lord")) {
-		
-	}else if(Foe.equalsIgnoreCase("What is te difference?")) {
 		
 	}
 }
